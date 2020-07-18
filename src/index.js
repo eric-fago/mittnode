b4cc519d87af5d526349475d919318d9d79189c5
Eric Fago <eric.fago@live.de>
2020-07-18 21:52:52 +0200

    routing modified using HashRouter (standard in not supported for gh pages)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
@@ -12,9 +12,9 @@ import './index.css';
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<BrowserRouter>
+			<HashRouter basename="/">
 				<App />
-			</BrowserRouter>
+			</HashRouter>
 		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
