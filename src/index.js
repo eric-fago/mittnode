de1f7beca6c602265cbcef250e232834c0bbab3e
Eric Fago <eric.fago@live.de>
2020-07-25 11:55:59 +0200

    router changed (no deploy to gh-pages)

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 import { persistor, store } from './redux/store';
-import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<HashRouter basename="/">
+			<BrowserRouter basename="/">
 				<PersistGate persistor={persistor}>
 					<App />
 				</PersistGate>
-			</HashRouter>
+			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
